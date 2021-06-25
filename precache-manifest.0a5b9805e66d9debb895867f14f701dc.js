b9af715722f67b553ad58d95e1ff1d6e106c371a
Jiacong Fang <zldrobit@126.com>
2021-06-25 15:35:21 +0800

    Updates

---
+++ precache-manifest.0a5b9805e66d9debb895867f14f701dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0580d789416dd2d97203",
+    "url": "/static/css/main.70927434.chunk.css"
+  },
+  {
+    "revision": "0580d789416dd2d97203",
+    "url": "/static/js/main.0580d789.chunk.js"
+  },
+  {
+    "revision": "2eb2a4e236a9ed2d2fa4",
+    "url": "/static/js/1.2eb2a4e2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d91e2f1a0effcbd32f819655f5817542",
+    "url": "/index.html"
+  }
+];
